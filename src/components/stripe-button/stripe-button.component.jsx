7da59da60a999b585e43decdca6391c4790c49ea
kaitrickster <kl3199@nyu.edu>
2020-01-13 00:07:12 +0800

    integrate stripe and update favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_4NVQfLCFafUdCwqH6d2LDQVQ00oH4EZrqc';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout 
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
